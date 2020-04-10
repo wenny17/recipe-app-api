727a412a6a48199d0b6c09c7be81a7089e558611
maks <temnepatypa17@gmail.com>
2020-04-10 19:42:10 +0300

    added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
     class Meta:
         model = get_user_model()
@@ -14,6 +14,17 @@ class UserSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
